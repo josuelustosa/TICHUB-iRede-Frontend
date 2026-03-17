d2eef85b6eccf9dc948bb020cedaaf17dd8bfb1b
Josué Lustosa <josuelustosa.job@gmail.com>
2026-03-16 22:17:55 -0400

    initial commit: base limpa projeto vue e estrutura inicial de App.vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      cart: {
+        total: 0,
+      },
+    }
+  },
+  methods: {
+    addItem() {
+      this.cart.total += 1
+    },
+    decrementItem() {
+      this.cart.total -= 1
+    },
+  },
+}
+</script>
+
+<template>
+  <main>
+    <article>
+      <h1>Carrinho</h1>
+      <p>Total de itens: {{ cart.total }}</p>
+
+      <button @click="addItem()">Adicionar</button>
+      <button @click="decrementItem()">Diminuir</button>
+    </article>
+  </main>
+</template>
+
+<style></style>
