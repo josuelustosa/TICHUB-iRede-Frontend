53403c31112af5139541e4ea364bfa0973748f22
Josué Lustosa <josuelustosa.job@gmail.com>
2026-03-23 14:36:31 -0400

    feat: criando uma classe anêmica e renderizando dados no template do vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script lang="ts">
+import { Product } from './model/product.model'
+
 export default {
   data() {
     return {
       cart: {
         total: 0,
       },
+      product: new Product(
+        'Camisa Casa Seleção Brasileira',
+        'Camisa 1 oficial da Seleção Brasileira',
+        750,
+        0.05,
+      ),
     }
   },
   methods: {
@@ -23,6 +31,20 @@ export default {
     <article>
       <h1>Carrinho</h1>
       <p>Total de itens: {{ cart.total }}</p>
+
+      <button @click="addItem()">Adicionar</button>
+      <button @click="decrementItem()">Diminuir</button>
+    </article>
+
+    <article>
+      <h1>{{ product.title }}</h1>
+      <p>{{ product.description }}</p>
+      <p>Valor original: R$ {{ product.price.toFixed(2).replace('.', ',') }}</p>
+      <p>
+        Valor com desconto ({{ product.discount }}%): R$
+        {{ product.price - product.price * product.discount }}
+      </p>
+      <p>Total de itens: {{ cart.total + 1 }}</p>
 
       <button @click="addItem()">Adicionar</button>
       <button @click="decrementItem()">Diminuir</button>
